dbe923a9bacbe8f0f85a2818c3f3a3daa99ed93b
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2022-06-23 17:16:38 +0200

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt install zsh -y
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i '/# DISABLE_AUTO_UPDATE="true"/c\DISABLE_AUTO_UPDATE="true"' ~/.zshrc
+sed -i '/plugins=(git)/c\plugins=(git zsh-autosuggestions)' ~/.zshrc
