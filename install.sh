b4b06d853c712cedaf0299a0c05b5681f4115b94
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2022-06-23 15:32:53 +0000

    Set default shell

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i '/# DISABLE_AUTO_UPDATE="true"/c\DISABLE_AUTO_UPDATE="true"' ~/.zshrc
 sed -i '/plugins=(git)/c\plugins=(git zsh-autosuggestions)' ~/.zshrc
+sudo chsh -s /bin/zsh
